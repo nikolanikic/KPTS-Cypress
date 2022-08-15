c9351da792a1d29f0cf6475d2aa2779f44468b5a
nikolanikic <nikolanikic11@gmail.com>
2022-08-15 19:09:25 +0200

    new SwagLabs tests, added pom

---
+++ cypress/support/KPTS/NikolaN/loginPage.js
@@ -0,0 +1,20 @@
+class loginPage{
+
+    usernameField(){
+        return cy.get('[data-test="username"]')
+    }
+
+    passwordField(){
+        return cy.get('[data-test="password"]')
+    }
+
+    loginButton(){
+        return cy.get('[data-test="login-button"]')
+    }
+
+    errorMessage(){
+        return cy.get('[data-test="error"]')
+    }
+
+}
+export default loginPage
