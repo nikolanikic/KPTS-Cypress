f91504e8c46ef29c8062e4801dedef0d1a4f638b
dzaja123 <nikolacardzajevic@gmail.com>
2022-08-23 18:30:57 +0200

    Added new tests

--- cypress/e2e/KPTS/NikolaDzajevic/demo_qa.cy.js
+++ cypress/e2e/KPTS/NikolaDzajevic/demo_qa.cy.js
@@ -6,11 +6,16 @@ describe('Demo QA', () => {
     const BasePage = new basePage()
     const ElementsPage = new elementsPage()
 
+    const fullName = 'Lazar'
+    const email = 'test.mail@gmail.com'
+    const currentAdress = 'Adresa'
+    const paymentAdress = 'Adresa'
+
     beforeEach(() => {
         cy.visit('https://demoqa.com')
     })
 
-    it('Web page load properly and elements are displayed properly', () => {
+    it('Web page load properly and elements are displayed properly.', () => {
         cy.get('header > a > img').should('be.visible')
         BasePage.elementsField().should('be.visible')
         BasePage.formsField().should('be.visible')
@@ -20,7 +25,38 @@ describe('Demo QA', () => {
         BasePage.bookField().should('be.visible')
     })
 
+    it('Navigate to Elements Page properly and check if elements are displayed.', () => {
+        BasePage.elementsField().click()
+        ElementsPage.elementsHeader().should('be.visible')
+        ElementsPage.formsHeader().should('be.visible')
+        ElementsPage.alertsHeader().should('be.visible')
+        ElementsPage.textBoxField().should('be.visible')
+        ElementsPage.checkBoxField().should('be.visible')
+        ElementsPage.radioButtonField().should('be.visible')
+    })
 
+    it('Navigate to Text Box field properly and check if elements are displayed.', () => {
+        BasePage.elementsField().click()
+        ElementsPage.textBoxField().click()
 
+        ElementsPage.fullNameField().should('be.visible')
+        ElementsPage.emailField().should('be.visible')
+        ElementsPage.currentAdressField().should('be.visible')
+        ElementsPage.paymentAdressField().should('be.visible')
+        ElementsPage.submitButton().should('be.visible')
+
+    })
+
+    it('User is able to submit payment on Elements Page.', () => {
+        BasePage.elementsField().click()
+        ElementsPage.textBoxField().click()
+
+        ElementsPage.fullNameField().type(fullName)
+        ElementsPage.emailField().type(email)
+        ElementsPage.currentAdressField().type(currentAdress)
+        ElementsPage.paymentAdressField().type(paymentAdress)
+        ElementsPage.submitButton().click()
+
+    })
 
 })
--- cypress/support/KPTS/NikolaDzajevic/elementsPage.js
+++ cypress/support/KPTS/NikolaDzajevic/elementsPage.js
@@ -1,26 +1,79 @@
 class elementsPage {
 
-    textBoxField() {
-        return cy.get('.category-cards > :nth-child(2)')
+    elementsHeader() {
+        return cy.get(':nth-child(1) > .group-header > .header-wrapper')
     }
 
-    formsField() {
-        return cy.get('.category-cards > :nth-child(2)')
+    textBoxField() {
+        return cy.get(':nth-child(1) > .element-list > .menu-list > #item-0')
     }
-    alertsField() {
-        return cy.get('.category-cards > :nth-child(3)')
+
+    fullNameField() {
+        return cy.get('#userName')
+    }
+
+    emailField() {
+        return cy.get('#userEmail')
+    }
+
+    currentAdressField() {
+        return cy.get('#currentAddress')
+    }
+
+    paymentAdressField() {
+        return cy.get('#permanentAddress')
+    }
+
+    submitButton() {
+        return cy.get('#submit')
+    }
+
+    submitedName() {
+        return cy.get('#name')
+    }
+
+    submitedEmail() {
+        return cy.get('#email')
+    }
+
+    submitedCurrentAdress() {
+        return cy.get('.border > #currentAddress')
+    }
+
+    submitedPaymentAdress() {
+        return cy.get('.border > #permanentAddress')
+    }
+
+    checkBoxField() {
+        return cy.get(':nth-child(1) > .element-list > .menu-list > #item-1')
+    }
+
+    radioButtonField() {
+        return cy.get(':nth-child(1) > .element-list > .menu-list > #item-2')
     }
 
     widgetsField() {
-        return cy.get('.category-cards > :nth-child(4)')
+        return cy.get(':nth-child(1) > .element-list > .menu-list > #item-3')
     }
 
     interactionsField() {
-        return cy.get('.category-cards > :nth-child(5)')
+        return cy.get(':nth-child(1) > .element-list > .menu-list > #item-5')
     }
 
     bookField() {
-        return cy.get('.category-cards > :nth-child(6)')
+        return cy.get(':nth-child(1) > .element-list > .menu-list > #item-6')
+    }
+
+    formsHeader() {
+        return cy.get(':nth-child(2) > .group-header > .header-wrapper')
+    }
+
+    alertsHeader() {
+        return cy.get(':nth-child(3) > .group-header > .header-wrapper')
+    }
+
+    widgetsHeader() {
+        return cy.get(':nth-child(4) > .group-header > .header-wrapper')
     }
 
 }
